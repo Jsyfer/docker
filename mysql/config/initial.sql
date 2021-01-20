c50f5e65ec44de4e89371502afe62de9832ec26b
Jsyfer <goldensundd@hotmail.com>
2021-01-20 11:38:09 +0800

    update mysql settings

--- mysql/config/initial.sql
+++ mysql/config/initial.sql
@@ -1,11 +1,14 @@
-CREATE TABLE `tbl_file` (
+create database If Not Exists `file_manager_system` default character set utf8 collate utf8_general_ci;
+use file_manager_system;
+
+CREATE TABLE IF NOT EXISTS `tbl_file` (
   `md5` varchar(32) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `path` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`md5`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-CREATE TABLE `tbl_tag` (
+CREATE TABLE IF NOT EXISTS `tbl_tag` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `group` varchar(45) DEFAULT NULL,
