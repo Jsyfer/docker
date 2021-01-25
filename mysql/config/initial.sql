66e4f1da4a6527ec4cfcbedca5e57c75ccac33df
Jsyfer <goldensundd@hotmail.com>
2021-01-25 12:01:41 +0800

    update sql

--- mysql/config/initial.sql
+++ mysql/config/initial.sql
@@ -2,15 +2,17 @@ create database If Not Exists `file_manager_system` default character set utf8 c
 use file_manager_system;
 
 CREATE TABLE IF NOT EXISTS `tbl_file` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
   `md5` varchar(32) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `path` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`md5`)
+  `name` varchar(255) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `thumb` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 CREATE TABLE IF NOT EXISTS `tbl_tag` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `group` varchar(45) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `group` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
--- mysql/readme.md
+++ mysql/readme.md
@@ -15,24 +15,3 @@ GRANT ALL PRIVILEGES ON *.* TO 'jsyfer'@'%' WITH GRANT OPTION;
 alter user 'jsyfer'@'%' identified with mysql_native_password by 'jsyfer';
 ```
 
-tagSyetem用SQL
-
-```sql
-create database If Not Exists `file_manager_system` default character set utf8 collate utf8_general_ci;
-use file_manager_system;
-
-CREATE TABLE IF NOT EXISTS `tbl_file` (
-  `md5` varchar(32) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `path` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`md5`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
-CREATE TABLE IF NOT EXISTS `tbl_tag` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `group` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
-```
-
