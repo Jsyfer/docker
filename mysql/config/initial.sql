2b06f73662afb001a8f303970bd3d47318749ecd
R5-2600 <goldensundd@hotmail.com>
2021-02-19 23:59:34 +0900

    update new table

--- mysql/config/initial.sql
+++ mysql/config/initial.sql
@@ -17,3 +17,19 @@ CREATE TABLE IF NOT EXISTS `tbl_tag` (
   `group` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE IF NOT EXISTS `file_manager_system`.`tbl_tagtofile` (
+  `md5` VARCHAR(32) NULL,
+  `tagid` INT NULL,
+  INDEX `md5_idx` (`md5` ASC) VISIBLE,
+  INDEX `tagid_idx` (`tagid` ASC) VISIBLE,
+  CONSTRAINT `md5`
+    FOREIGN KEY (`md5`)
+    REFERENCES `file_manager_system`.`tbl_file` (`md5`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `tagid`
+    FOREIGN KEY (`tagid`)
+    REFERENCES `file_manager_system`.`tbl_tag` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)ENGINE=InnoDB DEFAULT CHARSET=utf8
