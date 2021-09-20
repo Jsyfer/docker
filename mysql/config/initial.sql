f7c550c3ff4601bd18c6eb9a16aee5dbc2cd801e
Jsyfer <goldensundd@hotmail.com>
2021-09-20 14:46:20 +0800

    Update initial.sql

--- mysql/config/initial.sql
+++ mysql/config/initial.sql
@@ -2,36 +2,27 @@ create database If Not Exists `file_manager_system` default character set utf8 c
 use file_manager_system;
 
 CREATE TABLE IF NOT EXISTS `tbl_file` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `md5` varchar(32) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `md5` varchar(32) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `path` varchar(255) DEFAULT NULL,
-  `thumb` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`md5`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+  `thumb` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=141 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `tbl_tag` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `group` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `file_manager_system`.`tbl_tagtofile` (
-  `md5` VARCHAR(32) NOT NULL,
-  `tagid` INT NOT NULL,
-  INDEX `md5_idx` (`md5` ASC) VISIBLE,
-  INDEX `tagid_idx` (`tagid` ASC) VISIBLE,
-  CONSTRAINT `md5`
-    FOREIGN KEY (`md5`)
-    REFERENCES `file_manager_system`.`tbl_file` (`md5`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `tagid`
-    FOREIGN KEY (`tagid`)
-    REFERENCES `file_manager_system`.`tbl_tag` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    PRIMARY KEY (`md5`,`tagid`))ENGINE=InnoDB DEFAULT CHARSET=utf8
+CREATE TABLE IF NOT EXISTS `tbl_tagtofile` (
+  `fileid` int NOT NULL,
+  `tagid` int NOT NULL,
+  KEY `tagid_idx` (`tagid`),
+  KEY `fileid_idx` (`fileid`),
+  CONSTRAINT `fileid` FOREIGN KEY (`fileid`) REFERENCES `tbl_file` (`id`),
+  CONSTRAINT `tagid` FOREIGN KEY (`tagid`) REFERENCES `tbl_tag` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
